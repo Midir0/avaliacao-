d7d751ca156af9e50285b14f05670daa2ac13963
Midir <101377517+Midir0@users.noreply.github.com>
2022-03-17 12:47:20 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,35 @@
+let no1 = "Augusto Saravia Tavares";
+let no2 = "Bruno Ricardo Wotzke";
+let no3 = "Pedro Henrique Flores da Rocha";
+let no4 = "Alexis Reales Basilio";
+let no5 = "Isaque Muniz Guedes";
+
+let nu1 = 1
+let nu2 = 2
+let nu3 = 3
+let nu4 = 4
+let nu5 = 5
+
+console.log(nu1, nu2, nu3, nu4, nu5)
+
+let nu6 = true;
+let nu7 = false;
+let nu8 = true;
+let nu9 = false;
+let nu10 = true;
+
+console.log(nu6, nu7, nu8, nu9, nu10)
+
+let nu11 = 1234.3456
+let nu12 = 4567.4567
+let nu13 = 098765.34567
+let nu14 = 0987654321.123890
+let nu15 = 1234567890.1234567890
+
+console.log(nu11, nu12, nu13, nu14, nu15)
+
+const a16 = 1
+const bool = true
+const float = 1.5
+const string = "carlose"
+const a17 = 2
